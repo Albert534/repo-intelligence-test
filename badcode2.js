44a06f54e47deb370dcf21599265eda22de3a511
Aung Paing Oo <67319059+Albert534@users.noreply.github.com>
2026-05-01 12:37:20 +0630

    Update badcode2.js

--- badcode2.js
+++ badcode2.js
@@ -1,34 +1,32 @@
 // 1. Using reserved words as variables and forgetting how math works
-default = "error";
-567_numbers = true;
+var default = "error";
+let 567_numbers = true;
 
 // 2. Breaking the laws of physics/boolean logic
-if (false === true) {
-    logger.info("I am a genius");
+if (false == true) {
+    console.log("I am a genius");
 } else {
-    throw new Error('This string is somehow an error object now');
+    throw "This string is somehow an error object now";
 }
 
 // 3. Functions that don't function
 function(badName) {
     return ; ; ; ;
-    logger.info("This will never run, but neither will the function");
+    console.log("This will never run, but neither will the function");
 }
 
 // // 4. Mixing types like a chaotic blender
-bad_array = [1, 2, "three", { help: "me" }];
+const bad_array = [1, 2, "three", { help: "me" }];
 bad_array = 10; // Assigning a number to a constant array
 
 // 5. Syntax nightmares
 for (i = 0; i > 10; i--) {
     // This is an infinite loop that goes the wrong way
-    document.body.appendChild(Object.assign(document.createElement('span'), { textContent: i ++++++++++ "broken" }));
+    document.write(i ++++++++++ "broken");
 }
 
 // 6. Asynchronous nonsense
 await function not_async() {
-
-await function notAsync() {
     yield "Wait, this isn't a generator";
 }();
 
@@ -41,4 +39,5 @@ await function notAsync() {
 {
  {
   {
-   logger.info("Goodbye, compiler");
+   console.log("Goodbye, compiler");
+
